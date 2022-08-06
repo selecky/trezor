ba51fc8eb3ffeab7e7d1979c7c88518ef62d279d
tomas selecky <selecky@elkoep.cz>
2022-08-06 19:14:24 +0200

    16 - delete working 2

--- lib/strings/strings.dart
+++ lib/strings/strings.dart
@@ -2,7 +2,7 @@ class Strings {
 
   static String appTitle = 'Trezor';
   static String emptyCredListText = 'Your Trezor is empty.';
-  static String addCredentialsInfo = 'Click + and add some credentials.';
+  static String addCredentialsInfo = 'Click + to add some credentials.';
   static String addCredential = 'Add credential';
   static String editCredential = 'Edit credential';
   static String title = 'Title';
